254bb8239d6996ae06ae39dc9fcd46c8cc848888
shivajreddy <shivajreddy@outlook.com>
2024-05-23 13:35:37 -0400

    .

--- rust/playground/src/main.rs
+++ rust/playground/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {}
+
+struct Solution {}
+
+#[allow(unused)]
+impl Solution {
+    pub fn calculate(s: String) -> i32 {
+        println!("{}", s);
+        10
+    }
+}
+
+#[test]
+fn test_basic_calculator2_input1() {
+    let input = "3+2*2".to_string();
+    let expected_output = 7;
+
+    assert_eq!(Solution::calculate(input), expected_output);
+}
+
+#[test]
+fn test_basic_calculator2_input2() {
+    let input = "3+2*2".to_string();
+    let expected_output = 7;
+
+    assert_eq!(Solution::calculate(input), expected_output);
 }
