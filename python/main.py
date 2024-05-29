8ac1972bc8693852332aecb9747edc491d0f7f3c
shivajreddy <shivajreddy@outlook.com>
2024-05-29 09:45:59 -0400

    .

--- python/main.py
+++ python/main.py
@@ -12,7 +12,12 @@ A node can atmost have 1 node as its prev
 This ensures there is no branching or circular linked lists
 """
 class Node:
-    def __init__(self, key: int, val: int, next: 'Node | None', prev: 'Node | None') -> None:
+    def __init__(self,
+                 key: int,
+                 val: int,
+                 next: 'Node | None' = None,
+                 prev: 'Node | None' = None
+                 ) -> None:
         self.key = key
         self.val = val
         self.next = next
@@ -21,12 +26,15 @@ class Node:
 """
 
 head <-> node1 <-> node2 <-> ... <-> nodeN <-> tail
+
 """
 class DoublyLinkedList:
     def __init__(self) -> None:
-        pass
+        self.head = Node(-1, -1)
+        self.tail= Node(-1, -1)
 
-#
 
-
+class LRUCache:
+    def __init__(self, capacity: int) -> None:
+        self.capacity = capacity
 
