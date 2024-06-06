5444b431adeafef81ed0ffe6899c03fe91d6bd01
shivajreddy_wsl <shivajreddy@outlook.com>
2024-06-06 07:26:43 -0400

    .

--- rust/playground/src/main.rs
+++ rust/playground/src/main.rs
@@ -1,5 +1,19 @@
-mod doubly_linked_list;
-mod trie;
+// mod doubly_linked_list;
+// mod trie;
+#![allow(unused)]
 
-fn main() {}
+struct Node {
+    val: i32,
+    next: Option<Box<Node>>
+}
 
+
+fn main() {
+    let a  = Box::new(10);
+    let b: Box<[i32; 3]> = Box::new([10, 20, 30]);
+    let c = b[2];
+
+    println!("a={a} b ={b:?} c={c}");
+    println!("a={a:p} b ={b:p} c={:p}", &c as *const i32);
+    println!("a={a:p} b ={b:p} c={:p}", &c as *const i32);
+}
