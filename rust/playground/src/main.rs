5f3e0132558845d6054565f0a85f3a4dbea7c47f
shivajreddy_wsl <shivajreddy@outlook.com>
2024-06-11 14:57:59 -0400

    changes in playground from teclab

--- rust/playground/src/main.rs
+++ rust/playground/src/main.rs
@@ -1,45 +1,47 @@
+#![allow(unused)]
+
+use std::mem::{size_of, size_of_val};
+use std::rc::Rc;
+
+/*
+#[derive(Debug)]
+struct Node {
+    val: i32,
+    next: Option<Box<Node>>,
+}
+*/
+#[derive(Debug)]
+struct Node {
+    val: i32,
+    next: Option<Rc<Node>>,
+}
 
 
 fn main() {
-    
-    let s = "cbaebabacd".to_string();
-    let p = "abc".to_string();
-    
-    let mut p_map: [usize; 26] = [0; 26];
-    p.chars().for_each(|item| p_map[item as usize - 97] += 1);
-    
-    let mut result: Vec<i32> = Vec::new();
-    
-    let s_letters: Vec<char> = Vec::new();
-    
-    let (mut l, mut r) = (0, 0);
-    let mut win_map: [usize; 26] = [0; 26];
-    
-    while r < s.len() {
-        let right_idx = s_letters[r] as usize - 97;
-        win_map[right_idx] += 1;
-        
-        if r - l + 1 == p.len() {
-            
-            if same_list(&win_map, &p_map) {
-                result.push(l as i32);
-            }
-            
-            let left_idx = s_letters[l] as usize - 97;
-            win_map[left_idx] -= 1;
-            l += 1;
-        }
-        
-        r += 1;
-    }
+    /*
+    let mut n1 = Box::new(Node { val: 1, next: None });
+    let mut n2 = Box::new(Node { val: 2, next: None });
+    println!("{n1:p} {n1:?}");
+    println!("{n2:p} {n2:?}");
 
-}
+    n1.next = Some(n2);
+    println!("{n1:p} {n1:?}");
+    */
+    // let mut n1 = Rc::new(Node { val: 1, next: None });
+    let mut n1 = Node { val: 1, next: None };
+    let mut n4 = Node { val: 1, next: None };
+    let mut n2 = Rc::new(Node { val: 2, next: None });
+    let mut n3 = Rc::new(Node { val: 3, next: None });
+    println!("n1 {:p} {n1:?}", &n1);
+    println!("n2 {n2:p} {n2:?}");
 
-fn same_list(a: &[usize; 26], b: &[usize; 26]) -> bool {
-    for idx in (0..27) {
-        if a[idx] != b[idx] {
-            return false;
-        }
-    }
-    true
+    println!("{}", size_of::<Rc<Node>>());
+    println!("{}", size_of_val(&n2));
+    println!("StrongCount: {}", Rc::strong_count(&n2));
+
+    n1.next = Some(Rc::clone(&n2));
+    n4.next = Some(Rc::clone(&n2));
+    // println!("n1 {:p} {n1:?}", &n1);
+    println!("n2 {n2:p} {n2:?}");
+    println!("StrongCount: {}", Rc::strong_count(&n2));
 }
