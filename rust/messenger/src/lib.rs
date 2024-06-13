c4a3cbc1e6ac957e3f674a953bf9de860a7136f9
shivajreddy_wsl <shivajreddy@outlook.com>
2024-06-13 16:02:42 -0400

    learning refcell

--- rust/messenger/src/lib.rs
+++ rust/messenger/src/lib.rs
@@ -1,39 +1,76 @@
+#![allow(unused)]
+
 pub trait Messenger {
-    fn send(&self, msg: &str);
+    fn send(&self, message: &str);
 }
 
 pub struct LimitTracker<'a, T: Messenger> {
-    messenger: &'a T,
-    value: usize,
     max: usize,
+    value: usize,
+    messenger: &'a T
 }
+
 
 impl<'a, T: Messenger> LimitTracker<'a, T> {
-    pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
+    fn new(messenger: &'a T) -> LimitTracker<'a, T> {
         LimitTracker {
-            messenger,
+            max: 100,
             value: 0,
-            max,
+            messenger
         }
     }
-
-    pub fn set_value(&mut self, value: usize) {
+    
+    fn set_value(&mut self, value: usize) {
         self.value = value;
-
-        let percentage_of_max = self.value as f64 / self.max as f64;
-        if percentage_of_max >= 1.0 {
-            self.messenger.send("Error: You are over your quota!");
-        } else if percentage_of_max >= 0.9 {
-            self.messenger
-                .send("Urgent: You've used up over 90% of your quota!");
-        } else if percentage_of_max >= 0.75 {
-            self.messenger
-                .send("Warning: You've used up over 75% of your quota!");
+        let mut percent = self.value as f64 / self.max as f64;
+        if percent > 1.0 {
+            self.messenger.send("Over 1.0");
+        } else if percent > 0.9 {
+            self.messenger.send("Over 0.9");
+        } else if percent > 0.7 {
+            self.messenger.send("Over 0.7");
         }
     }
+    
 }
+
+
 
 #[cfg(test)]
 mod tests {
+    use std::rc::Rc;
+    use std::cell::RefCell;
     use super::*;
+    
+    struct MockMessenger {
+        // outbox: Vec<String>
+        outbox: Rc<RefCell<Vec<String>>>
+    }
+    
+    impl MockMessenger {
+        fn new() -> Self {
+            // MockMessenger {outbox: vec![]}
+            MockMessenger {outbox: Rc::new(RefCell::new(vec![]))}
+        }
+    }
+    
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            // self.outbox.push(String::from(message));
+            let rc1 = Rc::clone(&self.outbox);
+            let rc2 = Rc::clone(&self.outbox);
+            rc1.borrow_mut().push(String::from(message));
+            // rc2.borrow_mut().push(String::from(message));
+        }
+    }
+    
+    #[test]
+    fn test_message(){
+        let mut mock_messenger = MockMessenger::new();
+        
+        let mut tracker = LimitTracker::new(&mock_messenger);
+        tracker.set_value(90);
+        assert_eq!(mock_messenger.outbox.borrow().len(), 1);
+    }
 }
+
