057e14deb78e6a13fe8ec7343081afb6ef788559
shiva <shivajreddy@outlook.com>
2024-06-12 21:03:30 -0400

    refcell working

--- rust/messenger/src/lib.rs
+++ rust/messenger/src/lib.rs
@@ -1,14 +1,39 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![allow(unused)]
+
+trait Messenger {
+    fn send(&self, message: String);
+    // fn send(&self, message: &str);
 }
+
+
+
+struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,       // lives as long as this type instance
+    max: usize,
+    value: usize,
+}
+
+impl<'a, T: Messenger> LimitTracker<'a, T> {
+    fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
+        LimitTracker { messenger, value: 0, max}
+    }
+    
+    fn set_value(&mut self, value: usize) {
+        self.value = value;
+        let percent = self.value as f64 / self.max as f64;
+        if percent > 1.0 {
+            println!("OVER 100% : Banned");
+        } else if percent > 0.9 {
+            println!("OVER 90% : Soft Warning");
+        } else if percent > 0.7 {
+            println!("OVER 70% : Careful");
+        }
+    }
+}
+
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
