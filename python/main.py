09f01595aa97cce7916a7eea409e8b60eb40fca7
shiva <smbp@shiva.local>
2024-06-25 21:31:09 -0400

    .

--- python/main.py
+++ python/main.py
@@ -1,12 +1,3 @@
-s = "123 +1234 *"
 
-curr = 0
-
-curr_num = 0
-while curr < len(s) - 1 and s[curr].isdigit():
-    curr_num = (curr_num * 10) + int(s[curr])
-    curr += 1
-
-print(curr_num)
-
+print("hello")
 
