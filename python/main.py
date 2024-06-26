cc1a237ee6c4c4acaa22f3210bd2f9db5f951fc1
shiva <smbp@shiva.local>
2024-06-25 21:50:50 -0400

    .

--- python/main.py
+++ python/main.py
@@ -1,1 +1,22 @@
+import heapq
+
+input = [9, 7, 1, 3, 2, -1, 3, 5]
+print(input)
+
+arr = []
+
+heapq.heapify(arr)
+
+
+for num in input:
+    heapq.heappush(arr, -num)
+
+
+print(arr)
+
+while arr:
+    n = heapq.heappop(arr)
+    print(-n)
+
+
 
