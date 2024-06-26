70cfc8521b41cb090001c67d1e58b24ef4fab850
shivajreddy_wsl <shivajreddy@outlook.com>
2024-06-26 10:20:05 -0400

    monotonic queue

--- python/main.py
+++ python/main.py
@@ -1,22 +1,58 @@
-import heapq
+from collections import deque
 
-input = [9, 7, 1, 3, 2, -1, 3, 5]
-print(input)
+d = deque([])
 
-arr = []
+d.appendleft(10)
+d.append(20)
+d.appendleft(30)
 
-heapq.heapify(arr)
+print(d)
+
+#  Monotonic Queue
+'''
+    1  3  3  -1  -3  5  3  6  7
+    -
+    1
+
+    1  3  3  -1  -3  5  3  6  7
+    ----
+    3
+
+    1  3  3  -1  -3  5  3  6  7
+    -------
+    3,3
+
+    1  3  3  -1  -3  5  3  6  7
+       --------
+    3,3,-1  drop item not >= head
+
+    1  3  3  -1  -3  5  3  6  7
+          ---------
+    3,-1,-3  drop item >= head
+
+    1  3  3  -1  -3  5  3  6  7
+             ---------
+    5 new_item > head
+
+    1  3  3  -1  -3  5  3  6  7
+                 --------
+    5,3 drop_item not >= head
+
+    1  3  3  -1  -3  5  3  6  7
+                     -------
+    6 new_item > head
+
+    1  3  3  -1  -3  5  3  6  7
+                        -------
+    7 new_item > head
 
 
-for num in input:
-    heapq.heappush(arr, -num)
+    when shifting:
+        - if dropped item is highest item, pop head
+        - if new item strictly > highest, window holds only this item
+            else: pop off every item from back until new_item <= last item
 
 
-print(arr)
-
-while arr:
-    n = heapq.heappop(arr)
-    print(-n)
-
+'''
 
 
