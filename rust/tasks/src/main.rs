6eeb8f29555e6db681b8ef4df6487ff939562845
shiva <smbp@shiva.local>
2024-07-13 11:42:39 -0400

    .

--- rust/tasks/src/main.rs
+++ rust/tasks/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello There"
+}
+
+#[get("/")]
+fn all_tables() -> &'static str {
+    "All tables here"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/all", routes![all_tables])
 }
