3ad610a9e2c6f05fa2c003c0b0fc6dc5fe352525
shiva <smbp@shiva.local>
2024-07-13 10:02:31 -0400

    .

--- python/main.py
+++ python/main.py
@@ -1,21 +1,44 @@
-import heapq
-
-input = [9, 7, 1, 3, 2, -1, 3, 5]
-print(input)
-
-arr = []
-
-heapq.heapify(arr)
 
 
-for num in input:
-    heapq.heappush(arr, -num)
+sentence = "hellotherehowareyoudoingtoday"
+
+# create frequency
+
+# set
+
+'''
+- hashable items
+{  val1 val2 val2 }
+
+('a', 1)
+
+'''
+
+# list
+
+counter = [0] * 26
+
+# 'char' -> unicode, ASCII
+
+# aabccc
+# counter = [2 1 3 0 0 0 0 0 0 0 0 ]
+
+sentence = "hellotherehowareyoudoingtoday"
+for c in sentence:
+    # 1. find the position of this character in the counter
+    unicode_idx = ord(c)
+    idx = unicode_idx - ord('a')
+    counter[idx] += 1
 
 
-print(arr)
+# a: 0, b: 1
+letter = 'a'
+for count in counter:
+    print(letter, ":", count)
+    letter = chr(ord(letter) + 1)
 
-while arr:
-    n = heapq.heappop(arr)
-    print(-n)
+    
+
+
 
 
