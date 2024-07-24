247d0702b126adb631af1159b9348a21ffec57ee
shiva <shivajreddy@outlook.com>
2024-07-24 15:29:12 -0400

    ...

--- rust/playground2/src/main.rs
+++ rust/playground2/src/main.rs
@@ -1,3 +1,25 @@
+#![allow(unused)]
+use std::{
+    borrow::{Borrow, BorrowMut},
+    cell::RefCell,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let val = RefCell::new(42);
+
+    let mut_ref = val.borrow_mut();
+    let imm_ref = val.borrow();
+
+    println!("here");
+
+    let mut v2 = 42;
+    {
+        let mut mut_ref = &mut v2;
+
+        {
+            // println!("{:p}", &v2);
+        }
+
+        *mut_ref += 1;
+    }
 }
