4214d9370844273cb5a998acd730dd5ff4015487
shiva <shivajreddy@outlook.com>
2024-07-25 15:45:33 -0400

    .

--- rust/playground2/src/main.rs
+++ rust/playground2/src/main.rs
@@ -1,16 +1,1 @@
-fn main() {
-    let v = vec![Some(42), Some(43), None, Some(44)];
-    // let v = vec![Some(42), Some(43), Some(44)];
-
-    let mut idx = 0;
-
-    while idx < v.len() {
-        if let Some(val) = v[idx] {
-            println!("{}", val);
-            idx += 1;
-        } else {
-            println!("None");
-            idx += 1;
-        }
-    }
-}
+fn main() {}
