915bf07d28db5fb5ee8f6bc1e0cf333d976b1ddf
shiva <shivajreddy@outlook.com>
2024-07-25 16:09:18 -0400

    .

--- rust/playground/src/main.rs
+++ rust/playground/src/main.rs
@@ -15,17 +15,39 @@ impl<T> ListNode<T> {
 }
 
 struct LinkedList<T> {
-    root: Option<Node<T>>,
+    head: Option<Node<T>>,
     tail: Option<Node<T>>,
+    size: usize,
 }
 
 impl<T> LinkedList<T> {
     fn new() -> LinkedList<T> {
         LinkedList {
-            root: None,
+            head: None,
             tail: None,
+            size: 0,
         }
     }
+    fn push_node(&mut self, node: Node<T>) {
+        // add node to empty list
+        if self.size == 0 {
+            self.head = Some(node.clone());
+            self.tail = Some(node);
+            self.size += 1;
+        }
+        // add node
+        else {
+            // get prev tail
+            // let x = self.tail;
+            let prev_tail = self.tail.clone().unwrap();
+            prev_tail.borrow_mut().next = Some(node.clone());
+            // self.tail.unwrap().borrow_mut().next = Some(node.clone());
+            self.tail = Some(node);
+            self.size += 1;
+        }
+    }
+
+    fn push() {}
 }
 
 fn main() {
