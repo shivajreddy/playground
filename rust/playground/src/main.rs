7447e2ea323f91f925a88ee85b57ce15aaff9d05
shiva <shivajreddy@outlook.com>
2024-08-01 13:20:22 -0400

    .

--- rust/playground/src/main.rs
+++ rust/playground/src/main.rs
@@ -1,20 +1,3 @@
 #![allow(unused)]
 
-fn main() {
-    assert_eq!(
-        2,
-        Solution::longest_common_subsequence("bd".to_string(), "abcd".to_string())
-    );
-    assert_eq!(
-        2,
-        Solution::longest_common_subsequence("oxcpqrsvwf".to_string(), "shmtulqrypy".to_string())
-    );
-    assert_eq!(
-        3,
-        Solution::longest_common_subsequence("abcde".to_string(), "ace".to_string())
-    );
-}
-
-struct Solution {}
-
-impl Solution {}
+fn main() {}
