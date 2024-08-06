72a7920403f1a1e1ca130f194920490259a2f4b3
shiva <shivajreddy@outlook.com>
2024-08-06 17:10:38 -0400

    .

--- python/ground/main.py
+++ python/ground/main.py
@@ -1,2 +1,21 @@
-print("\t\tPython Plaground Result")
+"""
+sea
+i
+eat
+j
+
+if we find the longest 
+
+
+"""
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        return -1
+
+
+
+s = Solution()
+assert(2, s.minDistance("sea", "eat"))
+assert(4, s.minDistance("leetcode", "etco"))
 
